dd22b7130ce113240899914161ad31b6955fdcb2
Somaliika <lian.iryna@gmail.com>
2020-01-28 12:56:25 +0200

    Draft DynamoDB configuration in Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,34 @@ terraform {
 }
 
 
+module "base_label" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.11.1"
+  environment = "${var.environment}"
+  name        = "${var.application}"
+  tags        = "${merge(var.tags, map("Application", var.application))}"
+  enabled     = "${var.enabled}"
+}
+
+module "dynamodb_table" {
+  source = "git::https://github.com/cloudposse/terraform-aws-dynamodb?ref=tags/0.15.0"
+
+  name                          = "${var.application}-${var.environment}-dynamodb"
+  environment                   = "${var.environment}"
+  billing_mode                  = "PAY_PER_REQUEST"
+  enable_streams                = false
+  enable_encryption             = true
+  enable_point_in_time_recovery = true
+  hash_key                      = "path"
+  hash_key_type                 = "S"
+
+  dynamodb_attributes = [
+    {
+      name = "path"
+      type = "S"
+    },
+  ]
+
+  ttl_attribute = "expiration_date"
+
+  tags = "${module.base_label.tags}"
+}
