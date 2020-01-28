29f7de6f023708231255af0aeaa685083dded7ec
Somaliika <lian.iryna@gmail.com>
2020-01-28 13:19:10 +0200

    Adding some variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,21 @@ variable "environment" {
   type        = string
   description = "The logical environment where this is being deployed."
 }
+
+variable "application" {
+  type        = "string"
+  default     = "serverless"
+  description = "Solution name"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+}
+
+variable "enabled" {
+  type        = "string"
+  default     = "true"
+  description = "Set to false to prevent the module from creating any resources"
+}
