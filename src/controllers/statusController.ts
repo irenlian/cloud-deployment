944762a7a468f98768278b839b504de9b40ffbfc
Somaliika <lian.iryna@gmail.com>
2020-01-30 15:16:38 +0200

    Add version

--- src/controllers/statusController.ts
+++ src/controllers/statusController.ts
@@ -4,6 +4,7 @@ export const status = async (ctx: Koa.Context) => {
     ctx.status = 200;
     ctx.body = {
         status: 'ok',
+        version: 2,
         AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID
     };
 };
